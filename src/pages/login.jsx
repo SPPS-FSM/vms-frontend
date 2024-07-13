60d603181b069d900faed9d0ec29ebf1e40069a4
Naufal Randika <naufalrandika44@gmail.com>
2024-07-13 20:01:21 +0700

    login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,37 +4,54 @@ import {
   Input,
   Typography,
 } from "@material-tailwind/react";
-import React from "react";
-import { Navigate } from "react-router-dom";
-import Cookies from "js-cookie";
-import axios from "axios";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { login } from "../services/Auth";
 
 export default function Login() {
   const [formData, setFormData] = useState({
-    identifier:"",
-    password:""
-  })
-  const handleChange = (e)=>{
-    setFormData({...formData,[e.target.name]: e.target.value})
-  }
-  const handleSubmit = async()=>{
-    const data = {
-      identifier: formData.identifier,
-      password: formData.password
-    }
+    identifier: "",
+    password: ""
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:4000/api/auth/login", data)
-      console.log(response.data)
-      Cookies.set("accessToken", response.data.tokens.accessToken, {expires: 7})
-      Cookies.set("refreshToken", response.data.tokens.refreshToken, {expires: 7})
+      const role = await login(formData.identifier, formData.password);
+ 
+      switch(role) {
+        case 1:
+          navigate('/supplier/dashboard');
+          break;
+        case 2:
+          navigate('/staff/dashboard');
+          break;
+        case 3:
+          navigate('/manager/dashboard');
+          break;
+        case 4:
+          navigate('/admin/dashboard');
+          break;
+        default:
+          navigate('/login');
+      }
+      console.log(role);
     } catch (error) {
-      console.error(error.message)
+      console.error("Error login:", error);
     }
-  }
-  console.log(formData)
+  };
+
   return (
-    <div className=" h-screen bg-gradient-to-t from-wpigreen-50 to-wpiblue-50 flex items-center justify-center">
+    <div className="h-screen bg-gradient-to-t from-wpigreen-50 to-wpiblue-50 flex items-center justify-center">
       <Card
         className="p-8 border-2 border-gray-300 absolute"
         color="white"
@@ -46,7 +63,7 @@ export default function Login() {
         <Typography color="gray" className="mt-1 font-normal">
           Selamat datang di Vendor Management System PT WPI
         </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="h6" color="blue-gray" className="-mb-3">
               Email/NIM
@@ -55,7 +72,7 @@ export default function Login() {
               size="lg"
               placeholder="email"
               name="identifier"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className="!border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
@@ -69,14 +86,14 @@ export default function Login() {
               size="lg"
               placeholder="********"
               name="password"
-              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              className="!border-t-blue-gray-200 focus:!border-t-gray-900"
               labelProps={{
                 className: "before:content-none after:content-none",
               }}
               onChange={handleChange}
             />
           </div>
-          <Button className="bg-wpigreen-100 mt-6" fullWidth onClick={handleSubmit}>
+          <Button className="bg-wpigreen-100 mt-6" fullWidth type="submit">
             Login
           </Button>
           <a href="/register">
---
+++ src/services/Auth.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const API_URL = 'http://localhost:4000/api/auth/login';
+
+export const login = async (identifier, password) => {
+  try {
+    const response = await axios.post(API_URL, { identifier, password });
+
+    if (response.data.tokens) {
+      Cookies.set('accessToken', response.data.tokens.accessToken);
+      Cookies.set('refreshToken', response.data.tokens.refreshToken);
+      Cookies.set('id', response.data.user.id);
+
+      return response.data.user.role;
+    }
+    
+    return null;
+  } catch (error) {
+    console.error('Login gagal', error);
+    throw new Error('Login gagal');
+  }
+};
